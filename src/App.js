a0188d40576c08131eeb39c103be5d88239ea84e
Suzana dos Santos <94690066+suzanadossantos@users.noreply.github.com>
2023-05-23 13:54:48 -0300

    Página de edição de dados

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
 import Contact from './components/pages/Contact'
 import Projects from './components/pages/Projects'
+import Project from './components/pages/Project'
 
 import Container from './components/layout/Container';
 import NavBar from './components/layout/NavBar'
@@ -17,10 +18,11 @@ function App() {
       <Container customClass="min-height">
           <Routes>
             <Route path= "/" element={<Home/>}/> 
-            <Route path= "/Projects" element={<Projects/>}/> 
-            <Route path= "/Company" element={<Company/>}/> 
-            <Route path= "/Contact" element={<Contact/>}/> 
-            <Route path= "/NewProject" element={<NewProject/>}/>    
+            <Route path= "/projects" element={<Projects/>}/> 
+            <Route path= "/company" element={<Company/>}/> 
+            <Route path= "/contact" element={<Contact/>}/> 
+            <Route path= "/newproject" element={<NewProject/>}/> 
+            <Route path= "/project/:id" element={<Project/>}/>   
           </Routes>
         </Container>
       <Footer />
